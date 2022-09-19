cfb9d7a696b9f0c1621fc806ae9a965d2af4fd5d
rajputyash006 <rajputyash006@gmail.com>
2022-09-19 22:16:03 +0530

    skill and resume section change done

--- component/navbar.js
+++ component/navbar.js
@@ -3,7 +3,7 @@ let navbar = () => {
             <div><a href="index.html">Home</a></div>
             <div><a href="skills.html">Skills</a></div>
             <div><a href="project.html">Project</a></div>
-            <div><a href="https://drive.google.com/file/d/17Du8vojagimIfVyOm0zMdf19GvVd9S1f/view?usp=sharing">Resume</a></div>
+            <div><a href="./images/Yash-Rajput-Resume.pdf">Resume</a></div>
             <div><a href="contact.html">Contact</a></div>
         </div>`;
 };
